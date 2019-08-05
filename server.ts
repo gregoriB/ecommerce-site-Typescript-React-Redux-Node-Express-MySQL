846ab92f6e69b0751d0059b9a7b210acacbdac4a
gregoriB <brandon.gregori@gmail.com>
2019-08-04 20:53:45 -0700

    cleaned up code in server.ts

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express";
-import { MysqlError, FieldInfo } from "mysql";
+import { Response, NextFunction } from "express";
+import { MysqlError } from "mysql";
 
 const mysql = require("mysql");
 const express = require("express");
@@ -7,6 +7,7 @@ const app = express();
 const path = require("path");
 
 require("dotenv").config();
+
 const nodeEnv = app.get("env");
 const isDevelopment = nodeEnv === "development";
 
@@ -25,16 +26,30 @@ app.use((_: null, res: Response, next: NextFunction) => {
     next();
 });
 
-let shoppingCart = [];
 let products: any;
 
-app.post("/", (req: Request, res: Response) => {
-    // shoppingCart = req.body.cart;
-    // console.log(shoppingCart);
-    // res.json(shoppingCart);
+app.post("/", (_: null, res: Response) => {
     fetchProductData();
     res.json(products);
 });
+
+function fetchProductData() {
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    });
+    connection.connect();
+    connection.query(
+        "SELECT * FROM items",
+        (error: MysqlError, results: any) => {
+            if (error) throw error;
+            products = results;
+        }
+    );
+    connection.end();
+}
 
 const listener = app.listen(34567, () => {
     console.info("\x1b[33m", `${nodeEnv} server`);
@@ -45,24 +60,3 @@ const listener = app.listen(34567, () => {
         listener.address().port
     );
 });
-
-function fetchProductData() {
-    var connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-    });
-    connection.connect();
-    const query = connection.query(
-        "SELECT * FROM items",
-        (error: MysqlError, results: any, fields: FieldInfo[]) => {
-            if (error) throw error;
-            console.log(results);
-            products = results;
-        }
-    );
-    connection.end();
-
-    return query;
-}
