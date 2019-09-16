ed01a9620a4fecf46779065b3626ea5bd5596ed1
gregorib <brandon.gregori@gmail.com>
2019-09-16 00:16:25 -0700

    added console.log for troubleshooting

--- client/src/helpers/queryDatabase.ts
+++ client/src/helpers/queryDatabase.ts
@@ -9,6 +9,7 @@ const queryDatabase = async ({ path, query, method }: IQueryDBArgs): Promise<any
             headers: { "Content-Type": "applications/json" }
         };
     }
+    console.log(process.env.PORT);
     const response = await fetch(`${process.env.PORT}/${path}`, options);
     const data = await response.json();
     return data.rows.length ? data.rows : data;
