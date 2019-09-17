c2ef2519620a01da912e8d7e6fbb5d0bccf1db70
gregorib <brandon.gregori@gmail.com>
2019-09-17 14:25:08 -0700

    updated server.ts db credentials

--- server.ts
+++ server.ts
@@ -102,8 +102,11 @@ app.get("*", (req: Request, res: Response) => {
 });
 
 const dbCredentials: ConnectionConfig & ClientConfig = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT)
 };
 
 function queryDatabase(query: string, arr: string[], callback: Function) {
